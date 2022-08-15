4742342070cf60acf392148d4ef83a9694fe9906
NuminousBlue <jdiehl2236@gmail.com>
2022-08-15 14:10:41 -0500

    env vars

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
 import { ScheduleModule } from "@nestjs/schedule";
+import { MongooseModule } from "@nestjs/mongoose";
+
 import { RequestLogger } from "./common/middleware/logger.middleware";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [ScheduleModule.forRoot(), AuthModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
