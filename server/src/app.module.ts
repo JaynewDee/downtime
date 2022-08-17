634e288e4b4943a6e1a3b333ebcd5f063b641521
Joshua Diehl <jdiehl2236@gmail.com>
2022-08-16 21:50:25 -0500

    dotenv

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
 import { ScheduleModule } from "@nestjs/schedule";
 import { MongooseModule } from "@nestjs/mongoose";
-
+import { ConfigModule } from "@nestjs/config";
 import { RequestLogger } from "./common/middleware/logger.middleware";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
-
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
     AuthModule,
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    // MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
